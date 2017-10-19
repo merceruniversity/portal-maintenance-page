35d30bac32709f874d503c1e9d5d109a21b20b91
Todd Sayre <sayre_ts@mercer.edu>
2017-10-19 17:35:40 -0400

    File includes work

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var fileinclude = require('gulp-file-include'),
+gulp = require('gulp');
+
+gulp.task('fileinclude', function() {
+  gulp.src(['src/maintenance.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('build'));
+});
